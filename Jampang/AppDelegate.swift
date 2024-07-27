3f2a6387360b63c452e81135de15274349e879ad
Fitsyu <fitrahsyuhada@rocketmail.com>
2024-07-28 01:27:59 +0700

    Init project files

---
+++ Jampang/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Jampang
+//
+//  Created by Fitsyu  on 28/07/24.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
