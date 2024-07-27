abe8c75ade5ad443c772c26a86337f01b8fa7910
Fitsyu <fitrahsyuhada@rocketmail.com>
2024-07-28 06:00:06 +0700

    Implement the UI for Routes page

--- Jampang/AppDelegate.swift
+++ Jampang/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         let window = UIWindow(frame: UIScreen.main.bounds)
 
-        let hw = HomeWireframe()
+        let hw = RoutesWireframe()
         
         window.rootViewController = hw.viewController
         window.makeKeyAndVisible()
